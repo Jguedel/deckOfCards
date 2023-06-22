46874072c936f59eb85d6812f2ddde87f21b8a98
Jacob Guedel <57774270+Jguedel@users.noreply.github.com>
2023-06-22 17:25:38 +0000

    Update 3 files 6/22/23

--- src/Contol.js
+++ src/Contol.js
@@ -1,6 +1,7 @@
 import Card from "./Card";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { log } from "async";
 
 const Control = () => {
   //inital deck state
@@ -9,7 +10,7 @@ const Control = () => {
       deck_id: "",
       remaining: 0,
       shuffled: false,
-      success: false
+      success: false,
     };
     return empty;
   };
@@ -18,18 +19,19 @@ const Control = () => {
       cards: [
         {
           image:
-            "https://bicyclecards.org/wp-content/uploads/2019/11/red-56.jpg"
-        }
+            "https://bicyclecards.org/wp-content/uploads/2019/11/red-56.jpg",
+        },
       ],
       remaining: 0,
       shuffled: false,
-      success: false
+      success: false,
     };
     return empty;
   };
 
   const [show, setShow] = useState(true);
   const [clicked, setClicked] = useState(false);
+  const [restart, setRestart] = useState(false);
   const [card, setCard] = useState(emptyCard);
   const [deck, setDeck] = useState(emptyDeck);
 
@@ -47,6 +49,22 @@ const Control = () => {
     drawDeck();
   }, [show]);
 
+  //set deck on first render
+  useEffect(() => {
+    async function reDrawDeck() {
+      if (show) {
+        console.log("draw deck ran");
+        const { data } = await axios.get(
+          `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
+        );
+        setDeck((deck) => data);
+        console.log(deck);
+        console.log(clicked);
+      }
+    }
+    reDrawDeck();
+  }, [restart]);
+
   //set card draw
   useEffect(() => {
     const draw = async () => {
@@ -61,6 +79,8 @@ const Control = () => {
           setClicked((clicked) => false);
         }
         setCard((card) => data);
+      } else {
+        setCard((card) => emptyCard());
       }
     };
     draw();
@@ -75,28 +95,24 @@ const Control = () => {
     }
     const newDeck = {
       deck_id: deck.deck_id,
-      remaining: deck.remaining - 1,
+      remaining: deck.remaining,
       shuffled: true,
-      success: true
+      success: true,
     };
     setDeck((deck) => newDeck);
-    console.log(deck);
   };
   //handle restart event
   const handleRestart = (e) => {
     e.preventDefault();
-    const newDeck = emptyDeck();
-    setDeck((deck) => newDeck);
+    setClicked((click) => false);
+    setRestart((restart) => !restart);
     setShow((show) => true);
   };
 
   return (
     <div className="control">
-      {show ? (
-        <button onClick={handleCLick}>Darw a Card</button>
-      ) : (
-        <button onClick={handleRestart}>Restart</button>
-      )}
+      {show ? <button onClick={handleCLick}>Darw a Card</button> : null}
+      <button onClick={handleRestart}>Restart</button>
       {show ? <Card img={card} /> : <p className="outOfCards">out of cards</p>}
     </div>
   );
